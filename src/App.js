ff265ad99cb1b7740e459d812fc0372d5bed0a12
Ramal Abbassov <ramalabbasov044@gmail.com>
2023-05-23 11:09:12 +0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
-
+import "./App.css";
+import Home from "/Users/user/Desktop/travolic-new/src/pages/Home";
+import About from "/Users/user/Desktop/travolic-new/src/pages/About";
+import Contact from "/Users/user/Desktop/travolic-new/src/pages/Contact";
+import Services from "/Users/user/Desktop/travolic-new/src/pages/Services";
+import "./assests/css/reset.css";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import React from "react";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/services" element={<Services />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
